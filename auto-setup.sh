94bc5943fa05b9198d4b6b67a13a7c032ce610b2
root <guidoenr4@gmail.com>
2021-01-22 13:34:53 -0300

    deleted single files

--- auto-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# this is the guidoenr auto-setup for any dist of linux
-# set bash as default shell
-sudo chsh --shell /bin/bash
-
-# install gnome-tweaks
-sudo apt install gnome-tweaks
