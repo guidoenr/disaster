d7abd9847f443fd6a41637d22d57d59c091818dc
root <guidoenr4@gmail.com>
2021-01-22 13:34:00 -0300

    rename folders

---
+++ linux-setup/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this is the guidoenr auto-setup for any dist of linux
+# set bash as default shell
+sudo chsh --shell /bin/bash
+
+# install gnome-tweaks
+sudo apt install gnome-tweaks
