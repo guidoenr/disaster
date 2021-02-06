532e8a4c4876f740be50d371c0996534bd7f25b3
guidoenr <guidoenr4@gmail.com>
2021-02-06 11:47:23 -0300

    new commands

--- linux-setup/setup.sh
+++ linux-setup/setup.sh
@@ -13,3 +13,7 @@ sudo apt install gnome-tweaks
 # add color to the root@dist terminal, obs >> append
 echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
 source ~/.bashrc
+
+# O.S Screen prettt info
+sudo apt install neofetch
+sudo apt install screenfetch
