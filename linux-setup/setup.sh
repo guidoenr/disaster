d81f1d9d1ea0eec90220da31a5f8939540ef45a7
guidoenr <guidoenr4@gmail.com>
2021-05-02 19:01:47 -0300

    asd

--- linux-setup/setup.sh
+++ linux-setup/setup.sh
@@ -5,7 +5,7 @@
 sudo chsh --shell /bin/bash
 
 # install gnome-tweaks
-sudo apt install gnome-tweaks
+sudo apt install gnome-tweaks -y
 
 # add color to the root@dist terminal, obs >> append
 # non-root user: gedit .bashrc
@@ -15,9 +15,13 @@ PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[0
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
 # O.S Screen pretty info
-sudo apt install neofetch
-sudo apt install screenfetch
+sudo apt install neofetch -y
+sudo apt install screenfetch -y
 
 # Change the GRUB timeout in boot
 sudo gedit /etc/default/grub
-TIMEOUT=30
+
+#mpg123
+sudo apt install mpg123 -y
+
+#TIMEOUT=30
